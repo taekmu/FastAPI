86ab57b554fb5df8a9d890beb7bfb684e3c80742
TAEKMU <yongsacocori@gmail.com>
2026-02-11 14:22:46 +0900

    ai model change

--- server.py
+++ server.py
@@ -75,7 +75,7 @@ async def ask_ai(request: Request, user_input: str = Form(...)):
     if client:
         try:
             response = client.models.generate_content(
-                model="emini-3-flash-preview",
+                model="gemini-3-flash-preview",
                 contents=f"사용자가 이렇게 말했어: '{user_input}'. 이 사람에게 아주 다정하고 짧은 응원 메시지를 한 줄로 해줘."
             )
             ai_msg = response.text
